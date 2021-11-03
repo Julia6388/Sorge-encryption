19e7d8f3d6e4b164049af48ba211902ba7cbbeb8
Juju6388 <ulia210700@mail.ru>
2021-11-03 23:54:17 +0300

    Шифрование и дешифрование Рихарда Зорге

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(235, 318);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(497, 48);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(291, 134);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Рассшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Исходный текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(582, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Зашифрованный текст";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(497, 225);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(291, 141);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(582, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Расшифрованный текст";
+            //this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(272, 48);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(197, 109);
+            this.richTextBox4.TabIndex = 10;
+            this.richTextBox4.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Гамма";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 378);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            char[,] tab = new char[3, 10] { { 's', 'i', 'o', 'e', 'r', 'a', 't', 'n', '~', '~' },
+                { 'c', 'x', 'u', 'd', 'j', 'p', 'z', 'b', 'k', 'q' },
+                { '.', 'w', 'f', 'l', '/', 'g', 'm', 'y', 'h', 'v' }};
+            string text = richTextBox1.Text;
+            text = text.ToLower();
+            // заменим пробелы на /
+            text = text.Replace(' ', '/');
+            char[] tx = new char[text.Length];
+            string cip = null;
+            for (int i = 0; i < text.Length; i++)
+            {
+                tx[i] = Convert.ToChar(text[i]);
+                if (i - 1 != -1 && (Convert.ToInt32(tx[i - 1]) <= Convert.ToInt32('9') && 
+                    Convert.ToInt32(tx[i - 1]) >= Convert.ToInt32('0')) && (Convert.ToInt32(tx[i]) > Convert.ToInt32('9')
+                    || Convert.ToInt32(tx[i]) < Convert.ToInt32('0')))
+                    cip = cip + "94";               
+                if (Convert.ToInt32(tx[i]) <= Convert.ToInt32('9') && Convert.ToInt32(tx[i]) >= Convert.ToInt32('0'))
+                {
+                    if (i - 1 == -1)
+                         cip = cip + "94" + tx[i] + tx[i];
+                    if (i - 1 != -1 && (Convert.ToInt32(tx[i - 1]) > Convert.ToInt32('9') || 
+                        Convert.ToInt32(tx[i - 1]) < Convert.ToInt32('0')))
+                         cip = cip + "94" + tx[i] + tx[i];
+                    if (i - 1 != -1 && Convert.ToInt32(tx[i - 1]) <= Convert.ToInt32('9') && 
+                        Convert.ToInt32(tx[i - 1]) >= Convert.ToInt32('0'))
+                         cip = cip + tx[i] + tx[i];
+                    if (i + 1 == text.Length)
+                         cip = cip + "94";
+                }
+                if ((Convert.ToInt32(tx[i]) >= Convert.ToInt32('a') && Convert.ToInt32(tx[i]) <= Convert.ToInt32('z'))
+                || Convert.ToInt32(tx[i]) == Convert.ToInt32('.') ||
+                Convert.ToInt32(tx[i]) == Convert.ToInt32('/'))
+                {
+                    int x = -1, y = -1;
+                    for (int k = 0; k < 3; k++)
+                        for (int l = 0; l < 10; l++)
+                        {
+                            if (tx[i] == tab[k, l])
+                            {
+                                x = k;
+                                y = l;
+                                break;
+                            }
+                        }
+                    if (x == 0)
+                        cip += y;
+                    if (x == 1)
+                        cip += "8" + y;
+                    if (x == 2)
+                        cip += "9" + y;
+                }                
+            }
+            int[] cip1 = new int[cip.Length]; //клер
+            int[] cip2 = new int[cip.Length]; //гамма
+            for (int l = 0; l < cip.Length; l++)
+            {
+                cip1[l] = Convert.ToInt32(cip[l]) - Convert.ToInt32('0');
+                cip2[l] = Convert.ToInt32(richTextBox4.Text[l]) - Convert.ToInt32('0');
+            }
+            //сложение гаммы и клера
+            int[] cip3 = new int[cip.Length]; // шифр
+            for (int i = 0; i < cip.Length; i++)
+            {
+                if (cip1[i] + cip2[i] >= 10)
+                    cip3[i] = cip1[i] + cip2[i] - 10;
+                else cip3[i] = cip1[i] + cip2[i];
+                richTextBox2.Text += cip3[i];
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            char[,] tab = new char[3, 10] { { 's', 'i', 'o', 'e', 'r', 'a', 't', 'n', '~', '~' },
+                { 'c', 'x', 'u', 'd', 'j', 'p', 'z', 'b', 'k', 'g' },
+                { '.', 'w', 'f', 'l', '/', 'g', 'm', 'y', 'h', 'v' }};
+            char[] tx = new char[richTextBox2.Text.Length];
+            // Расшифровка
+            int[] cip1 = new int[richTextBox2.Text.Length]; //клер
+            int[] cip2 = new int[richTextBox2.Text.Length]; //гамма
+            int[] cip3 = new int[richTextBox2.Text.Length]; // шифр
+            for (int l = 0; l < richTextBox2.Text.Length; l++)
+            {
+                cip3[l] = Convert.ToInt32(richTextBox2.Text[l]) - Convert.ToInt32('0');
+                cip2[l] = Convert.ToInt32(richTextBox4.Text[l]) - Convert.ToInt32('0');
+            }
+            for (int i = 0; i < cip3.Length; i++)
+            {
+                if (cip3[i] - cip2[i] < 0)
+                    cip1[i] = 10 + cip3[i] - cip2[i];
+                else cip1[i] = cip3[i] - cip2[i];                
+            }
+            // разбиваем текст на части              
+            string str1 = null;
+            for (int i = 0; i < cip1.Length; i++)
+                str1 += cip1[i];            
+            str1 = str1.Replace("94", "*");
+            char[] str = str1.ToCharArray();
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == '*')
+                {
+                    int k = -1;
+                    for ( k = i; k < str.Length; k++)
+                        if (str[k] == '*')
+                        {
+                            if ((k - i) % 2 == 0)
+                                continue;
+                            bool p = false;
+                            for (int t = i + 2; t < k; t += 2)
+                            {
+                                if (str[t] == str[t - 1])
+                                    p = true;
+                                else p = false;
+                            }
+                            if (p)
+                            {
+                                str[i] = '#';
+                                str[k] = '#';
+                            }
+                            i = k;
+                        }
+                }
+            // расшифруем клер
+            string s = null;
+            for (int i=0; i< str.Length; i++)
+            {
+                int a = Convert.ToInt32(str[i]) - Convert.ToInt32('0');
+                if (a == 8)
+                {
+                    i++;
+                    int p= Convert.ToInt32(str[i]) - Convert.ToInt32('0');
+                    s += tab[1, p].ToString();
+                    continue;
+                }
+                if (a == 9 )
+                {
+                    i++;
+                    int p = Convert.ToInt32(str[i]) - Convert.ToInt32('0');
+                    s += tab[2, p].ToString();
+                    continue;
+                }
+                if (a >= 0 && a <= 7)
+                {
+                    s += tab[0, a].ToString();
+                    continue;
+                }
+                if (str[i] == '*' )
+                {
+                    s += ' '.ToString();
+                    continue;
+                }                
+                if (str[i] == '#')
+                {
+                    for ( i++; str[i]!='#'; i+=2 )
+                    s += str[i].ToString();                    
+                }
+            }
+            s = s.ToUpper();
+            richTextBox3.Text += s; 
+        }
+    }
+}
